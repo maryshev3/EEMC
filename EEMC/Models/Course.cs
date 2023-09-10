d9c54c17ca0f21561650600722acbc663e748dac
maryshev3 <maryshev3@gmail.com>
2023-09-10 17:02:34 +0300

    Initial commit

---
+++ EEMC/Models/Course.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EEMC.Models
+{
+    class Course
+    {
+        public string name = "";
+    }
+}
---
+++ EEMC/Models/JSONCoursesParser.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EEMC.Models
+{
+    class JSONCoursesParser
+    {
+        private List<Course> _courses = new List<Course>();
+
+        public List<Course> Courses 
+        {
+            get => _courses;
+        }
+
+        public void Parse() 
+        {
+
+        }
+    }
+}
