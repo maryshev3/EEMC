0fd7cd85b5226c18bfd51226799d65dbc2fb0097
maryshev3 <maryshev3@gmail.com>
2023-09-12 11:37:59 +0300

    Update 7
    Добавлена реакция на событие изменения католога "Курсы\..."
    До добавление DevExpress NuGet

--- EEMC/Models/Course.cs
+++ EEMC/Models/Course.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,6 +15,22 @@ namespace EEMC.Models
         public Course(ObservableCollection<Explorer>? Courses) 
         {
             _courses = Courses;
+
+            var watcher = new FileSystemWatcher(Path.Combine(Environment.CurrentDirectory, "Курсы"))
+            {
+                EnableRaisingEvents = true,
+                IncludeSubdirectories = true,
+            };
+
+            watcher.Changed += OnDirectoryChanged;
+            watcher.Created += OnDirectoryChanged;
+            watcher.Deleted += OnDirectoryChanged;
+            watcher.Renamed += OnDirectoryChanged;
+        }
+
+        private void OnDirectoryChanged(object sender, FileSystemEventArgs e) 
+        {
+            _courses = CourseBuilder.Build(new ExplorerBuilder())._courses;
         }
 
         public IEnumerable<Explorer> Courses 
