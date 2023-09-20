09ff1930334eef0e217de15ebe812912b129b10e
maryshev3 <maryshev3@gmail.com>
2023-09-20 21:57:32 +0300

    Update 14
    Добавлена возможность читать word файлы

---
+++ EEMC/ToXPSConverteres/WordConverter.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Office.Interop.Word;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Xps.Packaging;
+
+namespace EEMC.ToXPSConverteres
+{
+    public class WordConverter : IXPSConvert
+    {
+        public XpsDocument ToXpsConvert(string OriginFileName, string XPSFileName)
+        {
+            Microsoft.Office.Interop.Word.Application wordApplication = new Microsoft.Office.Interop.Word.Application();
+
+            wordApplication.Documents.Add(OriginFileName);
+
+            Document doc = wordApplication.ActiveDocument;
+
+            try
+            {
+                doc.SaveAs(XPSFileName, WdSaveFormat.wdFormatXPS);
+
+                wordApplication.Quit();
+
+                return new XpsDocument(XPSFileName, System.IO.FileAccess.Read);
+            }
+            catch (Exception exp)
+            {
+                string str = exp.Message;
+            }
+
+            return null;
+        }
+    }
+}
--- EEMC/ViewModels/CourseWindowVM.cs
+++ EEMC/ViewModels/CourseWindowVM.cs
@@ -2,23 +2,41 @@
 using EEMC.Messages;
 using EEMC.Models;
 using EEMC.Services;
+using EEMC.ToXPSConverteres;
+using EEMC.Views;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Xps.Packaging;
 
 namespace EEMC.ViewModels
 {
     public class CourseWindowVM : ViewModelBase
     {
+        private FixedDocumentSequence _document;
+
+        public FixedDocumentSequence Document 
+        {
+            get => _document;
+            set 
+            {
+                _document = value;
+                RaisePropertyChanged(() => Document);
+            }
+        }
+
         private Explorer _currentCourse;
 
         public Explorer CurrentCourse
         {
             get => _currentCourse;
-            set 
+            set
             {
                 _currentCourse = value;
                 RaisePropertyChanged(() => CurrentCourse);
@@ -27,7 +45,7 @@ namespace EEMC.ViewModels
 
         private readonly MessageBus _messageBus;
 
-        public CourseWindowVM(MessageBus messageBus) 
+        public CourseWindowVM(MessageBus messageBus)
         {
             _messageBus = messageBus;
 
@@ -36,5 +54,25 @@ namespace EEMC.ViewModels
                     CurrentCourse = message.Course;
                 });
         }
+
+        public ICommand ShowDocument 
+        {
+            get => new DelegateCommand((ChosenFile) =>
+            {
+                if (Path.GetExtension((ChosenFile as Explorer).NameWithPath) == ".docx") 
+                {
+                    //string newXPSDocumentName = String.Concat(System.IO.Path.GetDirectoryName(dlg.FileName), "\\",  System.IO.Path.GetFileNameWithoutExtension(dlg.FileName), ".xps");
+                    string OrigibDocumentName = Environment.CurrentDirectory + "\\" + (ChosenFile as Explorer).NameWithPath;
+
+                    string str = Path.Combine(Environment.CurrentDirectory, Path.GetFileNameWithoutExtension((ChosenFile as Explorer).Name) + ".xps");
+
+                    Document = new WordConverter().ToXpsConvert(OrigibDocumentName, Path.Combine(Environment.CurrentDirectory, Path.GetFileNameWithoutExtension((ChosenFile as Explorer).Name) + ".xps")).GetFixedDocumentSequence();
+                    //Document = new WordConverter().ToXpsConvert("D:\\netLessons\\EEMC\\EEMC\\bin\\Debug\\net6.0-windows\\Курсы\\Мат анализ\\Новая папка\\wr.docx", "D:\\netLessons\\EEMC\\EEMC\\bin\\Debug\\net6.0-windows\\Курсы\\Мат анализ\\Новая папка\\wr.xps");
+                    
+                    //MessageBox.Show(newXPSDocumentName);
+                }
+            });
+        }
+            
     }
 }
