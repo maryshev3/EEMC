fbf886b5227c5fb0fb4fe6209b71952a2a2a7c66
maryshev3 <maryshev3@gmail.com>
2024-03-22 10:33:49 +0300

    Сделал для VM по отображению курсов общий базовый класс + поправил отображение документов Word (при защищённом просмотре проблемы были)

--- EEMC/ViewModels/CoursesListVM.cs
+++ EEMC/ViewModels/CoursesListVM.cs
@@ -13,10 +13,9 @@ using System.Windows.Input;
 
 namespace EEMC.ViewModels
 {
-    public class CoursesListVM : ViewModelBase
+    public class CoursesListVM : CoursesListVMBase
     {
-        private Course _courses;
-        public Course Courses
+        public override Course Courses
         {
             get => _courses;
 
@@ -28,10 +27,7 @@ namespace EEMC.ViewModels
             }
         }
 
-        private readonly MessageBus _messageBus;
-        private Explorer? _chosenCourse = null;
-
-        private async void OnDirectoryChanged(object sender, FileSystemEventArgs e)
+        protected override async void OnDirectoryChanged(object sender, FileSystemEventArgs e)
         {
             string fileExt = Path.GetExtension(e.Name);
 
@@ -69,65 +65,7 @@ namespace EEMC.ViewModels
             _courses.AddWatcherHandler(OnDirectoryChanged);
         }
 
-        public ICommand AddCourse_Click
-        {
-            get => new Commands.DelegateCommand(async (obj) =>
-            {
-
-                Window window = new Window
-                {
-                    SizeToContent = SizeToContent.WidthAndHeight,
-                    ResizeMode = ResizeMode.NoResize,
-                    Title = "Добавление курса",
-                    Content = new AddCourse()
-                };
-
-                await _messageBus.SendTo<AddCourseVM>(new WindowMessage(window));
-
-                window.ShowDialog();
-            }
-            );
-        }
-
-        public ICommand RenameCourse_Click
-        {
-            get => new Commands.DelegateCommand(async (chosenCourse) =>
-            {
-                Window window = new Window
-                {
-                    SizeToContent = SizeToContent.WidthAndHeight,
-                    ResizeMode = ResizeMode.NoResize,
-                    Title = "Переименование курса",
-                    Content = new RenameCourse()
-                };
-
-                await _messageBus.SendTo<RenameCourseVM>(new ExplorerWindowMessage(window, chosenCourse as Explorer));
-
-                window.ShowDialog();
-            }
-            );
-        }
-
-        public ICommand RemoveCourse_Click
-        {
-            get => new Commands.DelegateCommand(async (chosenCourse) =>
-            {
-                Window window = new Window
-                {
-                    SizeToContent = SizeToContent.WidthAndHeight,
-                    ResizeMode = ResizeMode.NoResize,
-                    Title = "Удаление курса",
-                    Content = new RemoveCourse()
-                };
-
-                await _messageBus.SendTo<RemoveCourseVM>(new ExplorerWindowMessage(window, chosenCourse as Explorer));
-
-                window.ShowDialog();
-            }
-            );
-        }
-
-        public ICommand bMenu_Click
+        public override ICommand OpenCourse_Click
         {
             get => new Commands.DelegateCommand(async (ChosenCourse) =>
                 {
---
+++ EEMC/ViewModels/CoursesListVMBase.cs
@@ -0,0 +1,87 @@
+﻿using DevExpress.Mvvm;
+using EEMC.Messages;
+using EEMC.Models;
+using EEMC.Services;
+using EEMC.Views;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using System.Windows;
+
+namespace EEMC.ViewModels
+{
+    public abstract class CoursesListVMBase : ViewModelBase
+    {
+        protected Course _courses;
+        public abstract Course Courses { get; set; }
+
+        protected MessageBus _messageBus;
+        protected Explorer? _chosenCourse = null;
+
+        protected abstract void OnDirectoryChanged(object sender, FileSystemEventArgs e);
+
+        public ICommand AddCourse_Click
+        {
+            get => new Commands.DelegateCommand(async (obj) =>
+            {
+
+                Window window = new Window
+                {
+                    SizeToContent = SizeToContent.WidthAndHeight,
+                    ResizeMode = ResizeMode.NoResize,
+                    Title = "Добавление курса",
+                    Content = new AddCourse()
+                };
+
+                await _messageBus.SendTo<AddCourseVM>(new WindowMessage(window));
+
+                window.ShowDialog();
+            }
+            );
+        }
+
+        public ICommand RenameCourse_Click
+        {
+            get => new Commands.DelegateCommand(async (chosenCourse) =>
+            {
+                Window window = new Window
+                {
+                    SizeToContent = SizeToContent.WidthAndHeight,
+                    ResizeMode = ResizeMode.NoResize,
+                    Title = "Переименование курса",
+                    Content = new RenameCourse()
+                };
+
+                await _messageBus.SendTo<RenameCourseVM>(new ExplorerWindowMessage(window, chosenCourse as Explorer));
+
+                window.ShowDialog();
+            }
+            );
+        }
+
+        public ICommand RemoveCourse_Click
+        {
+            get => new Commands.DelegateCommand(async (chosenCourse) =>
+            {
+                Window window = new Window
+                {
+                    SizeToContent = SizeToContent.WidthAndHeight,
+                    ResizeMode = ResizeMode.NoResize,
+                    Title = "Удаление курса",
+                    Content = new RemoveCourse()
+                };
+
+                await _messageBus.SendTo<RemoveCourseVM>(new ExplorerWindowMessage(window, chosenCourse as Explorer));
+
+                window.ShowDialog();
+            }
+            );
+        }
+
+        public abstract ICommand OpenCourse_Click { get; }
+    }
+}
