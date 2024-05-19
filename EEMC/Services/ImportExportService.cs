c3637cefac7a836ea1d5d8ad00dfb764a4185411
maryshev3 <maryshev3@gmail.com>
2024-05-19 20:08:07 +0300

    Сделал методы для программы-просмотрщика

--- EEMC/Services/ImportExportService.cs
+++ EEMC/Services/ImportExportService.cs
@@ -321,5 +321,38 @@ namespace EEMC.Services
                 Directory.Delete(tmpDir, true);
             }
         }
+
+        private static void RemoveOldFiles()
+        {
+            if (Directory.Exists("./Файлы тем конвертированные"))
+                Directory.Delete("./Файлы тем конвертированные", true);
+
+            if (Directory.Exists("./Курсы конвертированные"))
+                Directory.Delete("./Курсы конвертированные", true);
+
+            if (Directory.Exists("./Курсы"))
+                Directory.Delete("./Курсы", true);
+
+            if (Directory.Exists("./Файлы тем"))
+                Directory.Delete("./Файлы тем", true);
+        }
+
+        public static void ImportForViewer(string cePath)
+        {
+            if (!File.Exists(cePath))
+                throw new Exception("Не удаётся найти переданный файл с курсами");
+
+            if (Path.GetExtension(cePath) != ".ce")
+                throw new Exception("Переданный файл имеет неверный формат");
+
+            RemoveOldFiles();
+
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+
+            using (ZipFile zip = ZipFile.Read(cePath, options: new ReadOptions() { Encoding = Encoding.UTF8 }))
+            {
+                zip.ExtractAll(Environment.CurrentDirectory, ExtractExistingFileAction.OverwriteSilently);
+            }
+        }
     }
 }
--- EEMC/ViewModels/ViewerBase.cs
+++ EEMC/ViewModels/ViewerBase.cs
@@ -50,6 +50,94 @@ namespace EEMC.ViewModels
 
         protected static CancellationTokenSource? _currentCancellationSource = null;
 
+        public Commands.IAsyncCommand ShowDocumentForViewer
+        {
+            get => new Commands.AsyncCommand(async (ChosenFile) =>
+            {
+                if (ChosenFile != null)
+                {
+                    XpsDocument oldXpsPackage = _xpsDocument;
+
+                    string fileExt = Path.GetExtension((ChosenFile as Explorer).NameWithPath);
+
+                    if (
+                        fileExt
+                            is ".docx"
+                            or ".doc"
+                            or ".txt"
+                            or ".cpp"
+                            or ".h"
+                            or ".py"
+                            or ".cs"
+                            or ".json"
+                            or ".xml"
+                            or ".html"
+                            or ".css"
+                            or ".ppt"
+                            or ".pptx"
+                    )
+                    {
+                        IsEnabledTW = false;
+
+                        string OriginDocumentName = Environment.CurrentDirectory + "\\" + (ChosenFile as Explorer).NameWithPath;
+
+                        if (_currentCancellationSource != null)
+                        {
+                            _currentCancellationSource?.Cancel();
+                            _currentCancellationSource?.Token.WaitHandle.WaitOne();
+
+                            _currentCancellationSource?.Dispose();
+
+                            _currentCancellationSource = null;
+                        }
+
+                        _currentCancellationSource = new CancellationTokenSource();
+
+                        try
+                        {
+                            StringBuilder filePath = new StringBuilder();
+
+                            filePath.Append(".\\");
+                            filePath.Append(
+                                (ChosenFile as Explorer)
+                                    .NameWithPath
+                                    .Replace("\\Курсы\\", "\\Курсы конвертированные\\")
+                                    .Replace("Файлы тем\\", "Файлы тем конвертированные\\")
+                            );
+                            filePath.Append(".xps");
+
+                            _xpsDocument = new XpsDocument(
+                                filePath.ToString(),
+                                FileAccess.Read
+                            );
+
+                        }
+                        catch (Exception ex)
+                        {
+                            MessageBox.Show(ex.Message);
+
+                            _xpsDocument = null;
+                        }
+                        _currentCancellationSource?.Dispose();
+                        _currentCancellationSource = null;
+
+                        //Сигнализирует о том, что было выполнено преобразование в xps
+                        if (!IsEnabledTW)
+                        {
+                            //null может быть, когда слишком быстро переключаешь окна (одно ещё не загрузилось, а второе уже включаем)
+                            if (_xpsDocument != null)
+                                Document = _xpsDocument.GetFixedDocumentSequence();
+
+                            oldXpsPackage?.Close();
+
+                            _xpsDocument?.Close();
+                            IsEnabledTW = true;
+                        }
+                    }
+                }
+            });
+        }
+
         public Commands.IAsyncCommand ShowDocument
         {
             get => new Commands.AsyncCommand(async (ChosenFile) =>
