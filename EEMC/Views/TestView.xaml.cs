32fc2c03ec3ef078c92b4c5a02a6478dc4cad49e
maryshev3 <maryshev3@gmail.com>
2024-05-26 18:03:19 +0300

    Добавил стиля просмотру тестов

--- EEMC/Views/TestView.xaml.cs
+++ EEMC/Views/TestView.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using EEMC.Models;
+using EEMC.ViewBases;
 using EEMC.ViewModels;
 using System;
 using System.Collections.Generic;
@@ -22,19 +23,47 @@ namespace EEMC.Views
     /// </summary>
     public partial class TestView : Window
     {
+        private Button _oldPressedButton;
+        private Button _oldHoveredButton;
+
         public TestView()
         {
             InitializeComponent();
         }
 
+        private void ResetButtonStyle(Button button)
+        {
+            if (button.BorderThickness.Bottom == 0)
+            {
+                button.BorderThickness = new Thickness() { Left = 0 };
+                button.BorderBrush = (SolidColorBrush)(new BrushConverter().ConvertFrom("#efeff5"));
+            }
+
+            button.Background = (SolidColorBrush)(new BrushConverter().ConvertFrom("#efeff5"));
+
+            if (button.Content is StackPanel)
+            {
+                var text = (button.Content as StackPanel).Children.OfType<Label>().First();
+                text.Foreground = System.Windows.Media.Brushes.Black;
+            }
+            else
+            {
+                var text = (button.Content as Label);
+                text.Foreground = System.Windows.Media.Brushes.Black;
+            }
+        }
+
         private void Question_Button_MouseEnter(object sender, MouseEventArgs e)
         {
-
+            HoverEffect(sender, ButtonType.CourseButton);
         }
 
         private void Question_Button_MouseLeave(object sender, MouseEventArgs e)
         {
-
+            if (_oldHoveredButton != default && _oldHoveredButton != _oldPressedButton)
+            {
+                ResetButtonStyle(_oldHoveredButton);
+            }
         }
 
         private void Question_Button_Click(object sender, RoutedEventArgs e)
@@ -45,6 +74,50 @@ namespace EEMC.Views
             dc.OpenQuestion.Execute((sender as Button).DataContext as Question);
 
             richTextBox.Document = dc.SelectedQuestion.QuestionText;
+
+            //Применяем стиль
+            if (_oldPressedButton != default)
+            {
+                ResetButtonStyle(_oldPressedButton);
+            }
+
+            Button button = sender as Button;
+
+            button.BorderThickness = new Thickness() { Left = 3 };
+            button.BorderBrush = (SolidColorBrush)(new BrushConverter().ConvertFrom("#4b6cdf"));
+
+            ColorSettings colorSettings = new ColorSettings(ButtonType.CourseButton);
+
+            var text = (button.Content as StackPanel).Children.OfType<Label>().First();
+            text.Foreground = (SolidColorBrush)(new BrushConverter().ConvertFrom(colorSettings.Foreground));
+
+            _oldPressedButton = button;
+        }
+
+        private void HoverEffect(object sender, ButtonType buttonType)
+        {
+            if (_oldHoveredButton != default && _oldHoveredButton != _oldPressedButton)
+            {
+                ResetButtonStyle(_oldHoveredButton);
+            }
+
+            Button button = sender as Button;
+
+            ColorSettings colorSettings = new ColorSettings(buttonType);
+            button.Background = (SolidColorBrush)(new BrushConverter().ConvertFrom(colorSettings.Background));
+
+            if (button.Content is StackPanel)
+            {
+                var text = (button.Content as StackPanel).Children.OfType<Label>().First();
+                text.Foreground = (SolidColorBrush)(new BrushConverter().ConvertFrom(colorSettings.Foreground));
+            }
+            else
+            {
+                var text = (button.Content as Label);
+                text.Foreground = (SolidColorBrush)(new BrushConverter().ConvertFrom(colorSettings.Foreground));
+            }
+
+            _oldHoveredButton = button;
         }
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
@@ -64,22 +137,22 @@ namespace EEMC.Views
 
         private void EndTest_Button_MouseEnter(object sender, MouseEventArgs e)
         {
-
+            Question_Button_MouseEnter(sender, e);
         }
 
         private void EndTest_Button_MouseLeave(object sender, MouseEventArgs e)
         {
-
+            Question_Button_MouseLeave(sender, e);
         }
 
         private void Conclusion_Button_MouseEnter(object sender, MouseEventArgs e)
         {
-
+            Question_Button_MouseEnter(sender, e);
         }
 
         private void Conclusion_Button_MouseLeave(object sender, MouseEventArgs e)
         {
-
+            Question_Button_MouseLeave(sender, e);
         }
     }
 }
