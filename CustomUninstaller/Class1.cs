ce0d138adc4278ee335b94dff63508d6ce781a6b
maryshev3 <maryshev3@gmail.com>
2024-06-12 20:50:24 +0300

    Добавил установщик. Добавил экран загрузки. Сделал кастомный uninstaller

---
+++ CustomUninstaller/Class1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomUninstaller
+{
+    [RunInstaller(true)]
+    public class MyUninstaller : System.Configuration.Install.Installer
+    {
+        public override void Uninstall(IDictionary savedState)
+        {
+            if (savedState != null)
+                base.Uninstall(savedState);
+
+            string currentPath = System.Reflection.Assembly.GetExecutingAssembly().Location;
+            string programPath = Path.GetFullPath(Path.Combine(currentPath, @"..\..\"));
+
+            string[] files = Directory.GetFiles(programPath);
+
+            // Сейчас находимся в директории установленной программы. Необходимо удалить все файлы, созданные не инсталлером.
+            // В их число входят xps файлы в каталоге с программой. Также всё содержимое папки "Курсы". Саму папку "Курсы" удалять не надо.
+            // Upd: необходимо удалять ещё папку EEMC.exe.WebView2
+            var filesToDelete = files.Where(x => Path.GetExtension(x) == ".xps");
+
+            var coursesPath = Path.Combine(programPath, "Курсы");
+
+            string[] courses = Directory.GetDirectories(coursesPath);
+
+            foreach (var course in courses)
+                Directory.Delete(course, true);
+
+            foreach (var file in filesToDelete)
+                File.Delete(file);
+
+            var webViewPath = Path.Combine(programPath, "EEMC.exe.WebView2");
+            Directory.Delete(webViewPath, true);
+        }
+    }
+}
--- EEMC/App.xaml.cs
+++ EEMC/App.xaml.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
+using System.IO;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 
@@ -15,6 +17,22 @@ namespace EEMC
     {
         protected override void OnStartup(StartupEventArgs e)
         {
+            Thread.Sleep(1000);
+
+            try
+            {
+                string currentPath = Environment.CurrentDirectory;
+                string coursePath = Path.Combine(currentPath, "Курсы");
+
+                if (!Directory.Exists(coursePath))
+                    Directory.CreateDirectory(coursePath);
+            }
+            catch
+            {
+                MessageBox.Show("Вы установили программу в папку, где требуются права администраторы для различных действий. В этом случае необходимо запустить программу от имени администратора");
+                return;
+            }
+
             ViewModelLocator.Init();
 
             base.OnStartup(e);
